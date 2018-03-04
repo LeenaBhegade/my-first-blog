568ddbeed62ab058368e830f5a6a7abae724010c
LeenaBhegade <bhegde.leena@gmail.com>
2018-03-04 16:21:06 +0000

    css changes;

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def add_comment_to_post(request, pk):
+    print('------hiiiiiiiiiii--------------')
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
